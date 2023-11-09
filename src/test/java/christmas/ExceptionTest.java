46b3d8be9cde7dccde5acbbf3d3c273647fde0ca
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-10 00:05:24 +0900

    test: 예약 날짜 예외 테스트 추가
    - 입력에 대한 예외 테스트 : 숫자가 아닌 경우에 대한 케이스
    - 등록에 대한 예외 테스트 : 12월에 포함된 일자가 아닌 경우에 대한 케이스

--- src/test/java/christmas/ExceptionTest.java
+++ src/test/java/christmas/ExceptionTest.java
@@ -8,6 +8,18 @@ import org.junit.jupiter.api.Test;
 
 public class ExceptionTest {
 
+    @DisplayName("방문일 입력 시, 입력이 없을 경우 예외가 발생함")
+    @Test
+    void inputDateExceptionByEmpty() {
+        //given
+        InputValidation inputValidation = new InputValidation();
+
+        //when, then
+        assertThatThrownBy(() -> inputValidation.validateNumber(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+
     @DisplayName("방문일 입력 시, 숫자가 입력되지 않을 경우 예외가 발생함")
     @Test
     void inputDateExceptionByNotNumber() {
@@ -16,6 +28,44 @@ public class ExceptionTest {
 
         //when, then
         assertThatThrownBy(() -> inputValidation.validateNumber("a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+
+    @DisplayName("방문일 입력 시, 공백이 있을 경우 예외가 발생함")
+    @Test
+    void inputDateExceptionByBlank() {
+        //given
+        InputValidation inputValidation = new InputValidation();
+
+        //when, then
+        assertThatThrownBy(() -> inputValidation.validateNumber("1 2"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+
+    @DisplayName("방문일 등록 시, 1보다 작은 경우 예외가 발생함")
+    @Test
+    void registerDateExceptionByUnderRange() {
+        //given
+        Calendar calendar = new Calendar();
+        Reservation reservation = new Reservation();
+
+        //when, then
+        assertThatThrownBy(() -> reservation.registerVisitDate(calendar, 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+
+    @DisplayName("방문일 등록 시, 31보다 큰 경우 예외가 발생함")
+    @Test
+    void registerDateExceptionByOverRange() {
+        //given
+        Calendar calendar = new Calendar();
+        Reservation reservation = new Reservation();
+
+        //when, then
+        assertThatThrownBy(() -> reservation.registerVisitDate(calendar, 32))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
