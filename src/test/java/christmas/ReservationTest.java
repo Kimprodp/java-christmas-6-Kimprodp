cc2852bd8644d8e953733040249e76af2c5c66ee
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-09 21:30:31 +0900

    test: 예약 날짜 등록 테스트 추가

---
+++ src/test/java/christmas/ReservationTest.java
@@ -0,0 +1,24 @@
+package christmas;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ReservationTest {
+
+    @DisplayName("예약한 날짜로 예약이 등록되어야 함")
+    @Test
+    void confirmReservationDateRegistration() {
+        //given
+        Calendar calendar = new Calendar();
+        Reservation reservation = new Reservation();
+        int date = 17;
+
+        //when
+        reservation.registerReservationDate(calendar, date);
+
+        //then
+        assertThat(reservation.getReservationDay()).isEqualTo(17);
+    }
+}
