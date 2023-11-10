e71294c4066e8cfa7d3aa969f70c737c14b5a846
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-10 15:07:00 +0900

    feat(InputProcessor): 빈 값 입력에 대한 예외처리 추가
    입력값이 숫자가 아닐 경우 예외 처리 되는 과정에서 빈 값 입력에 대한 경우도 같이 예외 처리 되고 있음
    빈 값 입력에 대한 예외 처리를 분리하여 추가

--- src/main/java/christmas/controller/InputProcessor.java
+++ src/main/java/christmas/controller/InputProcessor.java
@@ -1,15 +1,32 @@
 package christmas.controller;
 
 import christmas.view.ErrorMessage;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class InputProcessor {
 
+    private static final String COMMA = ",";
+    private static final String HYPHEN = "-";
+
     public int convertDate(String input) {
+        validateEmpty(input);
+        validateNumber(input);
+        return Integer.parseInt(input);
+    }
+
+    private void validateEmpty(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_EMPTY);
+        }
+    }
+
+    private void validateNumber(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.DATE_ERROR);
         }
-        return Integer.parseInt(input);
     }
 }
