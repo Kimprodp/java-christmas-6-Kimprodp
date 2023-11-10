5233a656c155e3bcdc1fd0f0a615baf80edd4c86
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-10 17:02:17 +0900

    test: 메뉴 주문 입력 예외 테스트 추가

--- src/test/java/christmas/ExceptionTest.java
+++ src/test/java/christmas/ExceptionTest.java
@@ -29,7 +29,7 @@ public class ExceptionTest {
         InputProcessor inputProcessor = new InputProcessor();
 
         //when, then
-        assertThatThrownBy(() -> inputProcessor.convertDate("1 2"))
+        assertThatThrownBy(() -> inputProcessor.convertDate(" "))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 입력값에 공백이 있습니다. 다시 입력해 주세요.");
     }
@@ -70,5 +70,53 @@ public class ExceptionTest {
         assertThatThrownBy(() -> reservation.registerVisitDate(calendar, 32))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+
+    @DisplayName("주문 메뉴 입력 시, 입력이 없을 경우 예외가 발생함")
+    @Test
+    void inputMenuExceptionByEmpty() {
+        //given
+        InputProcessor inputProcessor = new InputProcessor();
+
+        //when, then
+        assertThatThrownBy(() -> inputProcessor.convertMenu(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 입력값이 없습니다. 다시 입력해 주세요.");
+    }
+
+    @DisplayName("주문 메뉴 입력 시, 공백이 있을 경우 예외가 발생함")
+    @Test
+    void inputMenuExceptionByBlank() {
+        //given
+        InputProcessor inputProcessor = new InputProcessor();
+
+        //when, then
+        assertThatThrownBy(() -> inputProcessor.convertMenu(" "))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 입력값에 공백이 있습니다. 다시 입력해 주세요.");
+    }
+
+    @DisplayName("주문 메뉴 입력 시, 하이픈으로 구분되지 않을 경우 예외가 발생함")
+    @Test
+    void inputMenuExceptionByNotHyphen() {
+        //given
+        InputProcessor inputProcessor = new InputProcessor();
+
+        //when, then
+        assertThatThrownBy(() -> inputProcessor.convertMenu("해산물파스타2"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+
+    @DisplayName("주문 메뉴 입력 시, 주문 수량이 숫자가 아닐 경우 예외가 발생함")
+    @Test
+    void inputMenuExceptionByQuantityIsNotNumber() {
+        //given
+        InputProcessor inputProcessor = new InputProcessor();
+
+        //when, then
+        assertThatThrownBy(() -> inputProcessor.convertMenu("해산물파스타-a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
     }
 }
