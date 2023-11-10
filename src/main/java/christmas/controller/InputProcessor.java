635205c512f2bc7c0e6b2b88a6162cbb9d5c7242
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-10 15:11:58 +0900

    feat(InputProcessor): 공백 입력에 대한 예외처리 추가
    입력값이 숫자가 아닐 경우 예외 처리 되는 과정에서 공백 입력에 대한 경우도 같이 예외 처리 되고 있음
    공백 입력에 대한 예외 처리를 분리하여 추가

--- src/main/java/christmas/controller/InputProcessor.java
+++ src/main/java/christmas/controller/InputProcessor.java
@@ -7,11 +7,13 @@ import java.util.List;
 
 public class InputProcessor {
 
+    private static final String BLANK = " ";
     private static final String COMMA = ",";
     private static final String HYPHEN = "-";
 
     public int convertDate(String input) {
         validateEmpty(input);
+        validateBlank(input);
         validateNumber(input);
         return Integer.parseInt(input);
     }
@@ -19,6 +21,12 @@ public class InputProcessor {
     private void validateEmpty(String input) {
         if (input.isEmpty()) {
             throw new IllegalArgumentException(ErrorMessage.INPUT_EMPTY);
+        }
+    }
+
+    private void validateBlank(String input) {
+        if (input.contains(BLANK)) {
+            throw new IllegalArgumentException(ErrorMessage.CONTAIN_BLANK);
         }
     }
 
