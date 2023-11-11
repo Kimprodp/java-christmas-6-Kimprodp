710cd0a042912306e26b456cf38539f75c74fab4
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-11 16:03:42 +0900

    feat: 총 주문금액 출력 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -7,6 +7,7 @@ public class OutputView {
     private static final String EVENT_PREVIEW = "12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String ORDER_MENU = "<주문 메뉴>";
     private static final String ORDER_QUANTITY = "%s %d개";
+    private static final String ORDER_AMOUNT = "<할인 전 총주문 금액>%n%,d원";
 
     public void printEventPreview() {
         System.out.println(EVENT_PREVIEW);
@@ -18,5 +19,11 @@ public class OutputView {
         orderMenu.forEach((name, quantity) -> {
             System.out.println(String.format(ORDER_QUANTITY, name, quantity));
         });
+        System.out.println();
+    }
+
+    public void printOrderAmount(int amount) {
+        System.out.printf(ORDER_AMOUNT, amount);
+        System.out.println();
     }
 }
