606f496be8b988ba36bd10bbfd63dbc45d117719
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-13 11:44:20 +0900

    feat(Calendar): 이벤트들의 날짜 정보 추가

--- src/main/java/christmas/domain/Calendar.java
+++ src/main/java/christmas/domain/Calendar.java
@@ -1,6 +1,8 @@
 package christmas.domain;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,24 +11,28 @@ public class Calendar {
     protected static final int DEFAULT_YEAR = 2023;
     protected static final int DEFAULT_MONTH = 12;
     protected static final int DEFAULT_DAY = 1;
-    protected static final int LAST_DAY = 1;
+    protected static final int LAST_DAY = 31;
     protected static final int CHRISTMAS = 25;
 
-    protected final LocalDate startDate;
-    protected final LocalDate endDate;
-    protected final List<LocalDate> specialDay;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+    private final List<DayOfWeek> weekday;
+    private final List<DayOfWeek> weekend;
+    private final List<LocalDate> specialDay = new ArrayList<>();
 
 
     public Calendar() {
         startDate = LocalDate.of(DEFAULT_YEAR, DEFAULT_MONTH, DEFAULT_DAY);
         endDate = LocalDate.of(DEFAULT_YEAR, DEFAULT_MONTH, LAST_DAY);
-        specialDay = Arrays.asList(
-                LocalDate.of(DEFAULT_YEAR, DEFAULT_MONTH, 3),
-                LocalDate.of(DEFAULT_YEAR, DEFAULT_MONTH, 10),
-                LocalDate.of(DEFAULT_YEAR, DEFAULT_MONTH, 17),
-                LocalDate.of(DEFAULT_YEAR, DEFAULT_MONTH, 24),
-                LocalDate.of(DEFAULT_YEAR, DEFAULT_MONTH, 25),
-                LocalDate.of(DEFAULT_YEAR, DEFAULT_MONTH, 31));
+        weekday = Arrays.asList(DayOfWeek.SUNDAY, DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY,
+                DayOfWeek.THURSDAY);
+        weekend = Arrays.asList(DayOfWeek.FRIDAY, DayOfWeek.SATURDAY);
+
+        for (LocalDate date = startDate; date.isBefore(endDate.plusDays(1)); date = date.plusDays(1)) {
+            if (date.getDayOfWeek() == DayOfWeek.SUNDAY || date.getDayOfMonth() == CHRISTMAS) {
+                specialDay.add(date);
+            }
+        }
     }
 
     public int getYear() {
@@ -46,6 +52,14 @@ public class Calendar {
 
     public boolean isDateAvailable(LocalDate date) {
         return !date.isBefore(startDate) && !date.isAfter(endDate);
+    }
+
+    public boolean isWeekday(LocalDate date) {
+        return weekday.contains(date.getDayOfWeek());
+    }
+
+    public boolean isWeekend(LocalDate date) {
+        return weekend.contains(date.getDayOfWeek());
     }
 
     public boolean isSpecialDay(LocalDate date) {
