2ac98c9d8db386a20d6f4b071658a4502d9f1df1
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-14 00:21:18 +0900

    refactor(ChristmasDiscount): 크리스마스 할인 기능 리팩토링

--- src/main/java/christmas/domain/event/ChristmasDiscount.java
+++ src/main/java/christmas/domain/event/ChristmasDiscount.java
@@ -2,9 +2,8 @@ package christmas.domain.event;
 
 import christmas.domain.Calendar;
 import java.time.LocalDate;
-import java.util.HashMap;
 
-public class ChristmasDdayDiscount extends Event {
+public class ChristmasDiscount {
 
     private static final String EVENT_NAME = "크리스마스 디데이 할인";
     private static final int MIN_DISCOUNT = 1_000;
@@ -12,23 +11,25 @@ public class ChristmasDdayDiscount extends Event {
     private static final int DAILY_DISCOUNT = 100;
     private static final int TOTAL_DISCOUNT_DEFAULT = 0;
 
+    private final Calendar calendar;
     private int totalDiscount;
 
-    public ChristmasDdayDiscount(Calendar calendar) {
-        super(calendar);
+    public ChristmasDiscount(Calendar calendar) {
+        this.calendar = calendar;
         totalDiscount = TOTAL_DISCOUNT_DEFAULT;
     }
 
-    public HashMap<String, Integer> getBenefit(LocalDate date) {
+    public String getEventName() {
+        return EVENT_NAME;
+    }
+
+    public int getBenefit(LocalDate date) {
         if (isEventAvailable(date)) {
             applyDiscount(date);
         }
-        HashMap<String, Integer> benefitDetail = new HashMap<>();
-        benefitDetail.put(EVENT_NAME, totalDiscount);
-        return benefitDetail;
+        return totalDiscount;
     }
 
-    @Override
     protected boolean isEventAvailable(LocalDate date) {
         return calendar.isBeforeChristmas(date);
     }
