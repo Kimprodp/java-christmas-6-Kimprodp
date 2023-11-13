1f80b06522019a5e46db2f1df6a13eaaecfa8db3
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-14 00:19:44 +0900

    feat(EventBadge): 이벤트 배지 기능 추가

---
+++ src/main/java/christmas/domain/event/EventBadge.java
@@ -0,0 +1,58 @@
+package christmas.domain.event;
+
+import christmas.domain.Calendar;
+import java.time.LocalDate;
+import java.util.Arrays;
+
+public class EventBadge {
+
+    public enum Badge {
+        NONE("없음", 0),
+        STAR("별", 5_000),
+        TREE("트리", 10_000),
+        SANTA("산타", 20_000);
+
+        private final String badgeName;
+        private final int conditionAmount;
+
+        Badge(String badgeName, int conditionAmount) {
+            this.badgeName = badgeName;
+            this.conditionAmount = conditionAmount;
+        }
+
+        public String getBadgeName() {
+            return badgeName;
+        }
+
+        public int getConditionAmount() {
+            return conditionAmount;
+        }
+    }
+
+    private final Calendar calendar;
+    private Badge badge;
+
+    public EventBadge(Calendar calendar) {
+        this.calendar = calendar;
+    }
+
+    public String getBadgeName(Badge badge) {
+        return badge.getBadgeName();
+    }
+
+    public Badge getBenefit(LocalDate date, int benefitAmount) {
+        applyBadge(date, benefitAmount);
+        return badge;
+    }
+
+    private boolean isEventAvailable(LocalDate date) {
+        return calendar.isDecember(date);
+    }
+
+    private void applyBadge(LocalDate date, int benefitAmount) {
+        badge = Arrays.stream(Badge.values())
+                    .filter(badgeName -> benefitAmount >= badgeName.getConditionAmount() && isEventAvailable(date))
+                    .findFirst()
+                    .orElse(Badge.NONE);
+    }
+}
